cea8e46d29f39082a696d153e0c4f8273e5328f8
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-26 15:59:19 +0200

    feat: create custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=255, db_index=True)
+    last_name = models.CharField(max_length=255, db_index=True)
+    bio = models.TextField(default="This is my profile!", blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username

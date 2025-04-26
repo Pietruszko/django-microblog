1cb1dc58d32152fce3bc0f7da99a66c66076ea90
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-26 16:37:42 +0200

    feat: create media folder and configure settings for uploading avatars in DEBUG mode

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users.views import RegisterView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/register/', RegisterView.as_view(), name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

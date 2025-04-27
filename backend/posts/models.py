359be400ca9c3d1fda3a7eae19cad02abb0398d8
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-27 16:52:42 +0200

    feat: create post model with user relation

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content[:20]}'

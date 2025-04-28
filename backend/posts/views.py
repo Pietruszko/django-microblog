229ce9542af6de71411e68c621ea2c812b6eed3a
Pietruszko <gabriel.karalus98@gmail.com>
2025-04-28 12:10:00 +0200

    feat: create post viewset set up for router for basic CRUD operations and IsAuthenticatedOrReadOnly permission class

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all() # Default queryset (required for router)
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return super().get_queryset()

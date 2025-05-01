cf7a9e7f2085e6a2a4bbfdd80080a508dff1b4b2
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-01 09:10:16 +0200

    feat: add comment model (foreign key with user and post) and comment serializer

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -10,3 +10,14 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.user.username}: {self.content[:20]}'
+    
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content[:20]}'
--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,6 +9,18 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at': {'read_only': True}
         }
 
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+    
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'content', 'created_at']
+        extra_kwargs = {
+            'created_at': {'read_only': True}
+        }
+    
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
         return super().create(validated_data)

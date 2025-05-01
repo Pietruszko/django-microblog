3adbcafece2c1399dd3656edc9ccb960cc34fdc2
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-01 10:54:33 +0200

    chore: add related name to comment model

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -15,7 +15,7 @@ class Comment(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 

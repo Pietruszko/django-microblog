0a9d66b00ccc6bf8e8ae6c4748086611508b65eb
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-04 20:31:08 +0200

    feat: update user profile view set to normal modelviewset for adding editing user bio

--- backend/users/views.py
+++ backend/users/views.py
@@ -5,14 +5,25 @@ from .models import UserProfile
 from .serializers import UserProfileSerializer, CustomTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
-class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
+class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.select_related('user')
     serializer_class = UserProfileSerializer
-    lookup_field = 'user__id'
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    
+    def get_object(self):
+        if self.kwargs.get('pk') == 'me':
+            return self.request.user.userprofile
+        return super().get_object()
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer

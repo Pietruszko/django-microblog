58aab0ddf9cad42ae32f7176d7a7c0b0e98951f0
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-06 09:07:54 +0200

    feat: add default avatar to userprofile model and make migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,7 +5,7 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=255, db_index=True)
     last_name = models.CharField(max_length=255, db_index=True)
     bio = models.TextField(default="This is my profile!", blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default-avatar.png')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):

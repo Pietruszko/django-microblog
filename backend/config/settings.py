555f510231eb753c2b5b3f9cf8982b78195e1094
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-07 18:40:09 +0200

    fix: cors fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,6 +84,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://frontend',
+    'https://django-microblog-backend.onrender.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True

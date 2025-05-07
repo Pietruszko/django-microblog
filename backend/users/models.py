b5a05ff1bd30d84e9951aa8883448eef288f627a
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-07 22:01:22 +0200

    fix: change avatar to cloudinaryfield

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
     first_name = models.CharField(max_length=255, db_index=True)
     last_name = models.CharField(max_length=255, db_index=True)
     bio = models.TextField(default="This is my profile!", blank=True)
-    avatar = models.ImageField(
-    upload_to='avatars/',
-    default='avatars/default-avatar_rwky8f'
-    )
+    avatar = CloudinaryField('image', default='default-avatar_rwky8f')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):

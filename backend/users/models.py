82a7a69d4e44511236aeea7d0ac14e88cfa52f78
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-07 21:46:12 +0200

    fix: correct default avatar in model

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,7 +5,10 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=255, db_index=True)
     last_name = models.CharField(max_length=255, db_index=True)
     bio = models.TextField(default="This is my profile!", blank=True)
-    avatar = models.ImageField(upload_to='avatars/', default='avatars/default-avatar.png')
+    avatar = models.ImageField(
+    upload_to='avatars/',
+    default='avatars/default-avatar_rwky8f'
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):

fb788cd468da04dcad8003ba151913a8d533b75f
Pietruszko <gabriel.karalus98@gmail.com>
2025-05-07 19:20:44 +0200

    fix: add paths to cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -83,6 +83,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
     'http://frontend',
     'https://django-microblog-backend.onrender.com',
 ]
